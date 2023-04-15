f21cc59c305230f867a4687a2bbc5a3e6f2e7c2d
JonasArnold77 <105543936+JonasArnold77@users.noreply.github.com>
2023-04-15 16:00:15 +0200

    attack Improvement

--- Assets/Scripts/Combat/Attack.cs
+++ Assets/Scripts/Combat/Attack.cs
@@ -4,18 +4,62 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    private Animator _Animator;
+    private bool IsInAttackingTimeWindow;
+    private PlayerAnimation _PlayerAnimation;
+
     private void Start()
     {
         InventoryManager.Instance.ActualWeapon.GetComponent<Collider>().enabled = false;
+        _Animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (IsInAttackingTimeWindow)
+        {
+            if (CheckForComboTimeWindow())
+            {
+                //_PlayerAnimation.PlayNextAttack
+            }
+        }
     }
 
     public void SetColliderActive()
     {
         InventoryManager.Instance.ActualWeapon.GetComponent<Collider>().enabled = true;
+        IsInAttackingTimeWindow = true;
     }
 
     public void SetColliderInactive()
     {
         InventoryManager.Instance.ActualWeapon.GetComponent<Collider>().enabled = false;
+        IsInAttackingTimeWindow = false;
+    }
+
+    private bool CheckForComboTimeWindow()
+    {
+        if(GetCurrentAnimatorTime() >= GetDurationOfAnimatorClip() / 2 && )
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public float GetCurrentAnimatorTime(int layer = 2)
+    {
+        AnimatorStateInfo animState = _Animator.GetCurrentAnimatorStateInfo(layer);
+        float currentTime = animState.normalizedTime % 1;
+        return currentTime;
+    }
+
+    public float GetDurationOfAnimatorClip(int layer = 2)
+    {
+        AnimatorStateInfo animState = _Animator.GetCurrentAnimatorStateInfo(layer);
+        float duration = animState.length % 1;
+        return duration;
     }
 }
